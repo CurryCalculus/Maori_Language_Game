d6466fd98fb65fe73c944e6a23164eebbe90f95a
CurryCalculus <124116098+CurryCalculus@users.noreply.github.com>
2023-05-10 20:17:16 +1200

    Update 00_MLG_base_v2.py

--- .idea/00_MLG_base_v2.py
+++ .idea/00_MLG_base_v2.py
@@ -1,40 +1,5 @@
-"""MLG component - based on MLG base component 1
-I made this version because I couldn't make my assembled code working.
-"""
-
 import random
 
-# yes/no checking function
-def yes_no(question_text):
-    while True:
-
-        # Ask the user if they have played before
-        answer = input(question_text).lower()
-
-        # If they say yes, output 'Program Continues'
-        if answer == "yes" or answer == "y":
-            answer = "Yes"
-            return answer
-
-        # If they say no, output 'Display Instructions'
-        elif answer == "no" or answer == "n":
-            answer = "No"
-            return answer
-
-        # Otherwise - show error
-        else:
-            print("Please answer 'yes' or 'no")
-
-
-# function to display instructions
-def instructions():
-    print("***** How to Play *****")
-    print()
-    print("The rules of the game will go here")
-    print()
-
-
-# Define a function to get the quiz difficulty level from the user
 def get_quiz_difficulty():
     # Prompt user to choose quiz difficulty level
     difficulty = input("How difficult do you want the quiz to be? "
@@ -54,15 +19,67 @@ def get_quiz_difficulty():
             difficulty = input("Invalid input. "
                                "\nPlease choose a whole number between 1-3: ")
 
-
-# Main Routine go here...
-played_before = yes_no("Have you played this quiz before? ")
-
-if played_before == "No":
-    instructions()
 # Call the function to get the quiz difficulty level and store the result in a variable
 difficulty_level = get_quiz_difficulty()
-
 # Print a message indicating the user's chosen difficulty level
 print("You chose", difficulty_level, "difficulty.")
 
+def maori_numbers_quiz(difficulty_level):
+    maori_numbers = {
+        1: 'tahi',
+        2: 'rua',
+        3: 'toru',
+        4: 'wha',
+        5: 'rima',
+        6: 'ono',
+        7: 'whitu',
+        8: 'waru',
+        9: 'iwa',
+        10: 'tekau'
+    }
+
+    if difficulty_level == "easy":
+        max_number = 5
+    elif difficulty_level == "medium":
+        max_number = 7
+    else:
+        max_number = 10
+
+    consecutive_correct_answers = 0
+    num_questions_attempted = 0
+    num_correct_answers = 0
+    has_attempted_question = False
+
+    while True:
+        random_number = random.randint(1, max_number)
+        correct_answer = str(random_number)
+        user_input = input(f"What is '{maori_numbers[random_number]}' in English?: ")
+
+        if user_input.lower() == 'x':
+            if has_attempted_question:
+                print("Good job! Come back again after some rest!")
+                break
+            else:
+                print("You haven't attempted any questions yet. Please try answering a question first.")
+        else:
+            has_attempted_question = True
+            num_questions_attempted += 1
+
+            if user_input == correct_answer:
+                consecutive_correct_answers += 1
+                num_correct_answers += 1
+                if consecutive_correct_answers == 10:
+                    print("Well done! Keep up the hard work!")
+                else:
+                    print("Correct!")
+            else:
+                consecutive_correct_answers = 0
+                print(f"Wrong. The correct answer is '{correct_answer}'.")
+
+    accuracy_percentage = round(num_correct_answers/num_questions_attempted * 100, 2)
+    print(f"You attempted a total of {num_questions_attempted} questions.")
+    print(f"You got {num_correct_answers} questions correct before pressing 'x'.")
+    print(f"Your accuracy percentage is {accuracy_percentage}%.")
+
+# Call the function to run it.
+maori_numbers_quiz(difficulty_level)
